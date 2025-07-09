d0324134364f267cd9d6e0e1d1d9a15b9e390122
rinoBax <aderino123321@gmail.com>
2025-07-09 18:10:17 +0700

    update

---
+++ prisma/migrations/20250709110958_deployed_db/migration.sql
@@ -0,0 +1,135 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nama` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `picture` VARCHAR(191) NULL,
+    `nomorTelepon` VARCHAR(191) NULL,
+    `kecamatan` VARCHAR(191) NULL,
+    `domisili` VARCHAR(191) NULL,
+    `fotoKtp` VARCHAR(191) NULL,
+    `tglLahir` DATETIME(3) NULL,
+    `bankName` ENUM('EMPTY', 'BCA', 'MANDIRI', 'UOB', 'CIMB', 'BNI', 'BRI') NOT NULL DEFAULT 'EMPTY',
+    `noRekening` VARCHAR(191) NULL,
+    `kodeReferral` VARCHAR(191) NOT NULL,
+    `uplineId` INTEGER NULL,
+    `balance` DECIMAL(12, 2) NOT NULL DEFAULT 0,
+    `role` ENUM('USER', 'ADMIN', 'SUPER_ADMIN') NOT NULL DEFAULT 'USER',
+    `statusRegistrasi` ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
+    `previlegeStatus` BOOLEAN NOT NULL DEFAULT false,
+    `tglDibuat` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `tglDiperbarui` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_nomorTelepon_key`(`nomorTelepon`),
+    UNIQUE INDEX `users_kodeReferral_key`(`kodeReferral`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `projects` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `namaProyek` VARCHAR(191) NOT NULL,
+    `iconUrl` VARCHAR(191) NULL,
+    `nilaiProyek` DECIMAL(12, 2) NOT NULL,
+    `projectUrl` VARCHAR(191) NULL,
+    `deskripsi` VARCHAR(191) NULL,
+    `creatorId` INTEGER NOT NULL,
+    `tglDibuat` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `project_fields` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `label` VARCHAR(191) NOT NULL,
+    `fieldType` ENUM('TEXT', 'TEXTAREA', 'DROPDOWN', 'IMAGE', 'FILE') NOT NULL,
+    `isRequired` BOOLEAN NOT NULL DEFAULT true,
+    `projectId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `submissions` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `status` ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
+    `catatanAdmin` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `projectId` INTEGER NOT NULL,
+    `tglDibuat` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `submission_values` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `value` TEXT NOT NULL,
+    `submissionId` INTEGER NOT NULL,
+    `projectFieldId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `transactions` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tipe` ENUM('PENGERJAAN_PROYEK', 'KOMISI_UPLINE_1', 'KOMISI_UPLINE_2', 'PENARIKAN_DANA', 'BONUS_OPERASIONAL') NOT NULL,
+    `jumlah` DECIMAL(12, 2) NOT NULL,
+    `deskripsi` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `submissionId` INTEGER NULL,
+    `withdrawalId` INTEGER NULL,
+    `transactionDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `transactions_withdrawalId_key`(`withdrawalId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `withdrawals` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `totalWithdrawal` DECIMAL(12, 2) NOT NULL,
+    `status` ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
+    `userId` INTEGER NOT NULL,
+    `tglDiajukan` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `tglDiproses` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_uplineId_fkey` FOREIGN KEY (`uplineId`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `projects` ADD CONSTRAINT `projects_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `project_fields` ADD CONSTRAINT `project_fields_projectId_fkey` FOREIGN KEY (`projectId`) REFERENCES `projects`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `submissions` ADD CONSTRAINT `submissions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `submissions` ADD CONSTRAINT `submissions_projectId_fkey` FOREIGN KEY (`projectId`) REFERENCES `projects`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `submission_values` ADD CONSTRAINT `submission_values_submissionId_fkey` FOREIGN KEY (`submissionId`) REFERENCES `submissions`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `submission_values` ADD CONSTRAINT `submission_values_projectFieldId_fkey` FOREIGN KEY (`projectFieldId`) REFERENCES `project_fields`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `transactions` ADD CONSTRAINT `transactions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `transactions` ADD CONSTRAINT `transactions_submissionId_fkey` FOREIGN KEY (`submissionId`) REFERENCES `submissions`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `transactions` ADD CONSTRAINT `transactions_withdrawalId_fkey` FOREIGN KEY (`withdrawalId`) REFERENCES `withdrawals`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `withdrawals` ADD CONSTRAINT `withdrawals_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
